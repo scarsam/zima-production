84c92373be59ba763fb22c224e74283223d2de45
sam <s.ojling@gmail.com>
2020-11-14 18:27:13 +0100

    Updated env variables

--- utils/contentfulPosts.ts
+++ utils/contentfulPosts.ts
@@ -1,5 +1,5 @@
-const space = process.env.CONTENTFUL_SPACE_ID;
-const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN;
+const space = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID;
+const accessToken = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN;
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const client = require('contentful').createClient({
