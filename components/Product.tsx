b1321cfed24cf187b775edf8f260ab045e74e041
Max <maximilian.dennerlov@consid.se>
2020-12-29 13:45:57 +0100

    Add modal when clicking a product on supplier pages

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,5 +1,9 @@
+/* eslint-disable jsx-a11y/interactive-supports-focus */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import Image from 'next/image'
 import { ProductType } from 'types/allTypes'
+import Modal from 'components/shared/Modal'
+import { useState } from 'react'
 
 const Product: React.FC<ProductType & { hoverEffect?: boolean }> = ({
   metadata,
@@ -7,27 +11,48 @@ const Product: React.FC<ProductType & { hoverEffect?: boolean }> = ({
   hoverEffect = true,
 }) => {
   const { product_image } = metadata
+  const [showModal, setShowModal] = useState(false)
 
   return (
-    <div
-      className={`${
-        hoverEffect
-          ? 'transition transform duration-500 ease-in-out hover:-translate-y-1 hover:border-teal-500 hover:border-2 cursor-pointer'
-          : ''
-      } border bg-white border-gray-100 p-6 rounded-lg text-center shadow-md font-extrabold leading-10 tracking-tight`}
-    >
-      <div className="min-h-3/2">
-        <Image
-          src={`${product_image?.imgix_url}?q=&w=400`}
-          style={{ objectFit: 'contain', objectPosition: 'top' }}
-          alt={title}
-          width={200}
-          height={100}
-          layout="responsive"
-        />
-        <h2>{title}</h2>
+    <>
+      <div
+        role="button"
+        onClick={() => !hoverEffect && setShowModal(true)}
+        className={`${
+          hoverEffect
+            ? 'transition transform duration-500 ease-in-out hover:-translate-y-1 hover:border-teal-500 hover:border-2 cursor-pointer'
+            : ''
+        } border bg-white border-gray-100 p-6 rounded-lg text-center shadow-md font-extrabold leading-10 tracking-tight`}
+      >
+        <div>
+          <Image
+            src={`${product_image?.imgix_url}?q=&w=400`}
+            style={{ objectFit: 'contain', objectPosition: 'top' }}
+            alt={title}
+            width={200}
+            height={100}
+            layout="responsive"
+          />
+          <h2>{title}</h2>
+        </div>
       </div>
-    </div>
+      {showModal && (
+        <Modal
+          setShowModal={(show) => setShowModal(show)}
+          title={title}
+          image={
+            <Image
+              src={`${product_image?.imgix_url}?q=&w=400`}
+              style={{ objectFit: 'contain', objectPosition: 'top' }}
+              alt={title}
+              width={200}
+              height={100}
+              layout="responsive"
+            />
+          }
+        />
+      )}
+    </>
   )
 }
 
