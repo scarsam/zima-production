e1df164c2215eb81ebaea3de43166820dcf2c604
Max <maximilian.dennerlov@consid.se>
2021-01-09 16:13:34 +0100

    Add close button to modal

--- components/shared/Modal.tsx
+++ components/shared/Modal.tsx
@@ -11,20 +11,16 @@ interface IProps {
 const Modal: React.FC<IProps> = ({ setShowModal, image, title }: IProps) => {
   return (
     <>
-      <div
-        role="button"
-        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-        onClick={() => setShowModal(false)}
-      >
+      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
         <div className="relative w-full my-6 mx-auto max-w-3xl">
           <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-            <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
+            <div className="flex justify-between p-5 border-b border-solid border-gray-300 rounded-t items-center">
               <h3 className="text-1xl font-semibold">{title}</h3>
               <button
-                className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                className="shadow bg-teal-500 hover:bg-teal-200 focus:shadow-outline py-1 px-1 rounded-md ml-auto border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                 onClick={() => setShowModal(false)}
               >
-                <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                <span className="text-white h-6 w-6 text-2xl block outline-none focus:outline-none leading-5">
                   ×
                 </span>
               </button>
