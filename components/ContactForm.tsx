3c46c7133edb46c75d4fe6afdc383bb7a973c105
Max <maximilian.dennerlov@consid.se>
2021-01-09 17:25:02 +0100

    Add confirmation message after sending an email

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,12 +1,19 @@
-import { useRef, useEffect } from 'react'
+import { useRef, useEffect, useState } from 'react'
 import { ValidationError, useForm } from '@formspree/react'
 
 const ContactForm: React.FC = () => {
   const formRef = useRef(null)
   const [state, handleSubmit] = useForm('contactForm')
+  const [showConfirmationText, setShowConfirmationText] = useState(false)
 
   useEffect(() => {
     formRef?.current?.reset()
+    if (state.succeeded) {
+      setShowConfirmationText(true)
+      setTimeout(() => {
+        setShowConfirmationText(false)
+      }, 4000)
+    }
   }, [state.succeeded])
 
   return (
@@ -86,13 +93,35 @@ const ContactForm: React.FC = () => {
         </div>
       </div>
       <div className="md:flex md:items-center">
-        <div className="md:w-1/3">
+        <div className={`${showConfirmationText ? '' : 'md:w-1/3'}`}>
           <button
-            className="shadow disabled:opacity-50 disabled:cursor-wait disabled:bg-teal-500 bg-teal-500 hover:bg-green-800 focus:shadow-outline focus:outline-none text-white py-3 px-6 rounded-md"
+            className="shadow disabled:opacity-50 disabled:cursor-wait disabled:bg-teal-500 bg-teal-500 hover:bg-green-800 focus:shadow-outline focus:outline-none text-white py-3 px-4 rounded-md flex items-center"
             type="submit"
-            disabled={state.submitting}
+            disabled={state.submitting && !showConfirmationText}
           >
-            Skicka
+            {showConfirmationText ? 'Ditt meddelande har skickats' : 'Skicka'}
+            {state.submitting && !showConfirmationText && (
+              <svg
+                className="animate-spin ml-2 h-5 w-5 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+            )}
           </button>
         </div>
       </div>
